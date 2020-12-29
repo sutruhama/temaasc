76ceb05fc799fc0830493f6407fe3c8de7a48bbb
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:23:24 +0200

    add code

--- pool1.6/Program.cs
+++ pool1.6/Program.cs
@@ -10,6 +10,15 @@ namespace pool1._6
     {
         static void Main(string[] args)
         {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            if ((a < b + c) && (b < a + c) && (c < a + b))
+                Console.WriteLine("pot fi lungimile laturilor unui triunghi");
+            else
+                Console.WriteLine("nu pot fi lungimile laturilor unui triunghi");
+            Console.ReadKey();
         }
     }
 }
