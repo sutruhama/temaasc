655f561802e4224ce76fbf9da46709d1fcca8576
Ruhama Sut <sutruhama@yahoo.com>
2020-12-30 00:32:24 +0200

    add code

---
+++ pool2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,p=1;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+                p = p * i;
+            Console.WriteLine("Produsul este"+" "+p);
+            Console.ReadKey();
+        }
+    }
+}
