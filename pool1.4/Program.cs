07b704361cd6acda3d5136a3dc4b0952e23e3471
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:19:17 +0200

    add code

--- pool1.4/Program.cs
+++ pool1.4/Program.cs
@@ -10,6 +10,14 @@ namespace pool1._4
     {
         static void Main(string[] args)
         {
+            int y;
+            y = int.Parse(Console.ReadLine());
+            if ((y % 4 == 0) && (y % 100 != 0))
+                Console.WriteLine("anul este bisect");
+            if (y % 400 == 0)
+                Console.WriteLine("anul este bisect");
+            Console.WriteLine("anul nu este bisect");
+            Console.ReadKey();
         }
     }
 }
