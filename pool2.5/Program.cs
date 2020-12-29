8e3e0b00ce7968187a37369a3418e7019dcd8a05
Ruhama Sut <sutruhama@yahoo.com>
2020-12-30 00:51:42 +0200

    add code

---
+++ pool2.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, nreg = 0; ;
+            n = int.Parse(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x == i)
+                    nreg++;
+            }
+            Console.WriteLine("Exista" + " " + nreg + " " + "numere egale cu pozitia lor");
+            Console.ReadKey();
+        }
+    }
+}
