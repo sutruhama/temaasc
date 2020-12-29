12d4db73e109fde5ae2cf6b5ae1642ee5db2530e
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:24:48 +0200

    add code

--- pool1.7/Program.cs
+++ pool1.7/Program.cs
@@ -10,6 +10,14 @@ namespace pool1._7
     {
         static void Main(string[] args)
         {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("a=" + a + " " + "b=" + b);
+            Console.ReadKey();
         }
     }
 }
