6980972c57a2f359280a6aa664ce054cbd2da19a
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:30:22 +0200

    add code

---
+++ pool1.10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool1._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; bool ok = true;
+            n = int.Parse(Console.ReadLine());
+            if (n < 2)
+                Console.WriteLine("numarul" + " " + n + " " + "nu este prim");
+            for (int d = 2; d * d <= n; d++)
+            {
+                if (n % d == 0)
+                    ok = false;
+            }
+            if (ok)
+                Console.WriteLine("numarul" + " " + n + " " + "este prim");
+            Console.ReadKey();
+
+        }
+    }
+}
