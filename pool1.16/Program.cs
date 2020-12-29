cd596ecd5adf1249fc2791b732f8f32f10e218aa
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:40:33 +0200

    add code

---
+++ pool1.16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool1._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, aux;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            if (a > b) { aux = a; a = b; b = aux; }
+            if (a > c) { aux = a; a = c; c = aux; }
+            if (a > d) { aux = a; a = d; d = aux; }
+            if (a > e) { aux = a; a = e; e = aux; }
+            if (b > c) { aux = b; b = c; c = aux; }
+            if (b > d) { aux = b; b = d; d = aux; }
+            if (b > e) { aux = b; b = e; e = aux; }
+            if (c > d) { aux = c; c = d; d = aux; }
+            if (c > e) { aux = c; c = e; e = aux; }
+            Console.Write(a + " " + b + " " + c + " " + d + " " + e + " ");
+            Console.ReadKey();
+        }
+    }
+}
