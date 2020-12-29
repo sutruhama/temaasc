614dc517cbd455b2d5ef54d7b6c9c1235b983c37
Ruhama Sut <sutruhama@yahoo.com>
2020-12-30 00:23:32 +0200

    add code

---
+++ pool2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, nrpare = 0; ;
+            n = int.Parse(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x % 2 == 0)
+                    nrpare++;
+            }
+            Console.WriteLine("Exista" + " " + nrpare + " " + "numere pare");
+            Console.ReadKey();
+        }
+    }
+}
