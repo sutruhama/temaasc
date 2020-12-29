99e72ed296a7818e9d1eb5975bbf3e868663f31e
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:34:00 +0200

    add code

---
+++ pool1.12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool1._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a, b, c = 0; ;
+            n = int.Parse(Console.ReadLine());
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+            {
+                if (i % n == 0)
+                    c++;
+
+            }
+            Console.Write(c);
+            Console.ReadKey();
+        }
+    }
+}
