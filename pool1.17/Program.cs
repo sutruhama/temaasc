92e8c2deddb4a26d2495d3a8585ebdd45926b4e4
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:42:38 +0200

    add code

---
+++ pool1.17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool1._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int c = a * b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            while (b != 0)
+            {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+            Console.WriteLine("cel mai mare divizor comun este" + " " + a);
+            Console.WriteLine("cel mai mic multiplu comun este" + " " + c / a);
+            Console.ReadKey();
+        }
+    }
+}
