1016529ef9e786de5c46e0b62dcb91d650389804
Ruhama Sut <sutruhama@yahoo.com>
2020-12-30 01:54:12 +0200

    add code

---
+++ pool2.7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, max = -1, min=999999;
+            n = int.Parse(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                if (x > max)
+                    max = x;
+                else
+                    if (x < min)
+                    min = x;
+            }
+            Console.WriteLine("maximul este" + " " + max);
+            Console.WriteLine("minimul este" + " " + min);
+            Console.ReadKey();
+        }
+    }
+}
