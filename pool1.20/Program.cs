03fc33213b4850d655c688f89406d99c2342df68
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 21:43:08 +0200

    add code

---
+++ pool1.20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool1._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            float y = m / (float)n;
+            Console.WriteLine(y);
+            Console.ReadKey();
+            
+        }
+    }
+}
