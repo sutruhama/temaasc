7e9b346348457f8729a01e9c5915e3493dd3b5dc
Ruhama Sut <sutruhama@yahoo.com>
2020-12-30 00:47:30 +0200

    add code

---
+++ pool2.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a;  bool ok=false;
+            n = int.Parse(Console.ReadLine());
+            a = int.Parse(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x == a)
+                {
+                    ok = true;
+                    Console.WriteLine("pozitia este" + " " + i);
+                }
+            }
+            if (ok == false)
+                Console.WriteLine(-1);
+            Console.ReadKey();
+        }
+    }
+}
