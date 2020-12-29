0e1fc94889f12fcda279a4655558c534018e153f
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:44:15 +0200

    add code

---
+++ pool1.18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool1._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, d, p;
+            n = int.Parse(Console.ReadLine());
+            d = 2;
+            while (n > 0)
+            {
+                p = 0;
+                while (n % d == 0)
+                {
+                    p++;
+                    n = n / d;
+                }
+                if (p > 0)
+                    Console.Write(d + " " + " ^ " + " " + p + " ");
+                d++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
