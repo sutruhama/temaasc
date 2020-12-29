067d6e20eb16bb66795d87924d324946e4524901
Ruhama Sut <sutruhama@yahoo.com>
2020-12-29 19:28:47 +0200

    add code

---
+++ pool1.9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool1._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, d;
+            n = int.Parse(Console.ReadLine());
+            for (d = 1; d <= n; d++)
+            {
+                if (n % d == 0)
+                    Console.Write(d + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
