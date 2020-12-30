c6142534e4eb2d1d3611117fe69d608364e6b8da
Ruhama Sut <sutruhama@yahoo.com>
2020-12-30 09:31:54 +0200

    add code

---
+++ pool2.8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("n = ");
+            n = int.Parse(Console.ReadLine());
+            int nr_2=0, nr_1, nr_0;
+            nr_0 = 1;
+            nr_1 = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                nr_2 = nr_1 + nr_0;
+                nr_0 = nr_1;
+                nr_1 = nr_2;
+                
+            }
+            Console.WriteLine(nr_2);
+            Console.ReadKey();
+        }
+    }
+
+}
