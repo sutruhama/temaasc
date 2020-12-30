a63c9a89af5b802079d3531f86accf16a792b3a9
Ruhama Sut <sutruhama@yahoo.com>
2020-12-30 22:11:39 +0200

    add code

---
+++ pool2.10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1, longestcount = 0, longestnr = 0;
+            int n = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                
+                int b = int.Parse(Console.ReadLine());
+                if (b != a)
+                    count = 0;
+                count++;
+                if (count > longestcount)
+                {
+                    longestcount = count;
+                    longestnr =b;
+                }
+                a = b;
+            }
+            Console.WriteLine("Numarul maxim de numere egale conescutive este"+" "+longestcount);
+            Console.ReadKey();
+        }
+    }
+}
