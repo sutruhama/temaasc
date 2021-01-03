c3f4baff5c5bc0c9ea9b28e9f6de99a3f3c9ab4a
Ruhama Sut <sutruhama@yahoo.com>
2021-01-03 15:09:30 +0200

    add code

---
+++ pool3.7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            for (int i = 0; i < n; i++)
+                v[i] = int.Parse(Console.ReadLine());
+            int[] temp = v;
+            Console.Write("Original Array: ");
+            foreach (int i in v)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Array.Reverse(temp);
+            Console.Write("Reversed Array: ");
+            foreach (int i in temp)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
