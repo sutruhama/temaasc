f35408a09a6ed1e5c4e687526f433c0fe011f51f
Ruhama Sut <sutruhama@yahoo.com>
2021-01-03 16:08:15 +0200

    add comit

---
+++ pool3.9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool3._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            int[] b=new int[n];
+            int index;
+            int place;
+            for (int i = 0; i < n; i++)
+                v[i] = int.Parse(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+            {
+                index = i - k;
+                place = n + index;
+                if (index >= 0)
+                {
+                    b[index] = v[i];
+                }
+                else
+                {
+                    b[place] = v[i];
+                }
+            }
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(b[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
