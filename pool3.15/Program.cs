fae23893f16a56fa3abf82974b0d5a71c8c5e5bb
Ruhama Sut <sutruhama@yahoo.com>
2021-01-03 19:55:51 +0200

    add code

---
+++ pool3.15/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool3._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j <n; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        for (int k = j; k < n; k++)
+                        {
+                            if (k != n - 1)
+                            {
+                                int temp = a[k];
+                                a[k] = a[k + 1];
+                                a[k + 1] = temp;
+
+                            }
+                        }
+                        j--;
+                        n--;
+                    }
+                }
+
+            }
+            for (int i = 0; i < n; i++)
+                Console.Write(a[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
