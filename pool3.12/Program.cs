be9d448100c96e36e4fe66c8a038ba6254cd3e4d
Ruhama Sut <sutruhama@yahoo.com>
2021-01-03 17:39:42 +0200

    add code

---
+++ pool3.12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool3._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            for (int i = 0; i < n; i++)
+                v[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Selection sort");
+            Console.Write("Vectorul initial este: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+            int temp, min;
+            for (int i = 0; i < n - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (v[j] < v[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = v[min];
+                v[min] = v[i];
+                v[i] = temp;
+            }
+            Console.WriteLine();
+            Console.Write("Vectorul sortat este: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
