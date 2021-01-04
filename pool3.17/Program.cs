8affcc27dcc6a6fff75c2a512c73057e897201b7
Ruhama Sut <sutruhama@yahoo.com>
2021-01-04 08:17:33 +0200

    add code

---
+++ pool3.17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace pool3._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int num, baseNum;
+            Console.Write("Enter a decimal number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a base: ");
+            baseNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num + " converts to ");
+            MulBase(num, baseNum);
+            Console.WriteLine(" Base " + baseNum);
+            Console.Read();
+        }
+        static void MulBase(int n, int b)
+        {
+            Stack Digits = new Stack();
+            do
+            {
+                Digits.Push(n % b);
+                n /= b;
+            } while (n != 0);
+            while (Digits.Count > 0)
+                Console.Write(Digits.Pop());
+        }
+    }
+}
