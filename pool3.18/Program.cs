7e6a3f7f07ea8fe991e30d6015e0a577691677e8
Ruhama Sut <sutruhama@yahoo.com>
2021-01-16 15:43:18 +0200

    add code

---
+++ pool3.18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool3._18
+{
+    class Program
+    {
+        public static double F(List<double> coeffs, double x)
+        {
+            double total = 0;
+            double x_factor = 1;
+            for (int i = 0; i < coeffs.Count; i++)
+            {
+                total += x_factor * coeffs[i];
+                x_factor *= x;
+            }
+            return total;
+        }
+    }
+}
