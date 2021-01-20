a2f2c9e2afe6aec1b75e2fe4530ed64a21e69f21
Ruhama Sut <sutruhama@yahoo.com>
2021-01-20 13:05:14 +0200

    add code

---
+++ pool3.19/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool3._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce the size of first array");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce the size of second array");
+            int n2 = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n1];
+            int[] arr2 = new int[n2];
+            int k = 0;
+            Console.WriteLine("Introduce the values of first array");
+            for (int i = 0; i < n1; i++)
+                arr1[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce the values of second array");
+            for (int i = 0; i < n1; i++)
+                arr2[i] = int.Parse(Console.ReadLine());
+            for (int i = 0; i <n1; i++)
+            {
+
+                for (int j = 0; j < n2; j++)
+                {
+
+                    if (arr1[i] == arr2[j])
+                    {
+                        k++;
+                        break;
+                    }
+
+                }
+
+            }
+            if (n1 == k)
+            {
+                Console.WriteLine(true);
+            }
+            else
+                Console.WriteLine(false);
+            Console.ReadKey();
+        }
+    }
+    }
+
---
+++ pool3.31/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool3._31
+{
+    class Program
+    {
+        static void findMajority(int[] arr, int n)
+        {
+            int maxCount = 0;
+            int index = -1; 
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i] == arr[j])
+                        count++;
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    index = i;
+                }
+            }
+            if (maxCount > n / 2)
+                Console.WriteLine(arr[index]);
+
+            else
+                Console.WriteLine("No Majority Element");
+        }
+
+        static public void Main()
+        {
+
+            int[] arr = { 1, 1, 2, 1, 3, 5, 1 };
+            int n = arr.Length;
+            findMajority(arr, n);
+        }
+    }
+}
+
